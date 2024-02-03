d92fbeda192ed8469d8d28cbb8fbccd216eb74ca
Polina Puchek <polinapuchek89@gmail.com>
2024-02-03 12:17:42 +0200

    factorial

--- utils.py
+++ utils.py
@@ -5,5 +5,9 @@ def g(z):
         i = i*5
         s += 1
     return s
-z = int(input())
-print(g(z))
+
+def f(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * f(n - 1)
